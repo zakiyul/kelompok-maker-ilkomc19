579018d804633f5ccc48e0267cb6314960e45c95
zakiyul <zakiyul.py@gmail.com>
2021-05-14 15:33:58 +0800

    adding rel=noreferer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,21 @@ const Footer = () => {
     <footer className="bg-dark p-4 text-center text-white mt-3">
       Copyright &copy; 2021 <br />
       <div className="d-flex justify-content-center">
-        <a className="h4" href="https://twitter.com/zekiul" target="_blank">
+        <a
+          className="h4"
+          // rel="noopener"
+          rel="noreferrer"
+          href="https://twitter.com/zekiul"
+          target="_blank"
+        >
           <AiOutlineTwitter />
         </a>
         <a
           className="ms-4 me-4 h4"
           href="https://facebook.com/eeki.py"
           target="_blank"
+          // rel="noopener"
+          rel="noreferrer"
         >
           <AiFillFacebook />
         </a>
@@ -23,6 +31,8 @@ const Footer = () => {
           className="h4"
           href="https://www.instagram.com/zzzakiul/"
           target="_blank"
+          // rel="noopener"
+          rel="noreferrer"
         >
           <AiFillInstagram />
         </a>
